fd4fe1bbe02bf4df0ab0cfc6d4640eaa57734e4b
Florian Klampfer <mail@qwtel.com>
2022-04-15 13:25:31 +0700

    Commit

--- index.ts
+++ index.ts
@@ -1,18 +1,18 @@
 // deno-lint-ignore-file no-explicit-any
-import { ResolvablePromise } from 'https://ghuc.cc/worker-tools/resolvable-promise/index.ts';
+import { ResolvablePromise } from 'https://deno.land/x/resolvable_promise/index.ts';
 
 // const queueMicrotask = typeof globalThis.queueMicrotask === "function"
 //   ? globalThis.queueMicrotask
 //   : (callback: VoidFunction) => Promise.resolve().then(callback).catch(e => setTimeout(() => { throw e }));
 
-export class ExtendablePromise<T = unknown> /* extends Promise<T[]> */ implements Promise<PromiseSettledResult<T>[]> {
+export class ExtendablePromise<T = unknown> implements Promise<PromiseSettledResult<T>[]> {
   #values: PromiseSettledResult<T>[] = [];
   #promise: ResolvablePromise<PromiseSettledResult<T>[]>;
   #numAdded = 0;
   #numSettled = 0;
 
   constructor(f?: T | PromiseLike<T>) {
-    // super(r => r(undefined as any));
+    // super(_ => _(void 0 as any));
     this.#promise = new ResolvablePromise();
     this.waitUntil(f);
     // queueMicrotask(() => {
@@ -37,12 +37,7 @@ export class ExtendablePromise<T = unknown> /* extends Promise<T[]> */ implement
   }
 
   waitUntil(f?: T | PromiseLike<T>) {
-    // if ((<any>globalThis).process?.env?.NODE_ENV === 'development' || (<any>globalThis).DEBUG) {
-    //   if (this.#promise.settled) {
-    //     console.warn("Can't add promise to an ExtendablePromise that has already settled. This is a no-op");
-    //   }
-    // }
-    if (f) {
+    if (f != null) {
       const i = this.#numAdded;
       Promise.resolve(f)
         .then(v => this.#fulfill(i, v), r => this.#reject(i, r))
@@ -50,6 +45,7 @@ export class ExtendablePromise<T = unknown> /* extends Promise<T[]> */ implement
     }
   }
 
+  /** @deprecated Name of this property might change */
   get settled() { return this.#promise.settled }
 
   then<TResult1 = PromiseSettledResult<T>[], TResult2 = never>(onfulfilled?: ((value: PromiseSettledResult<T>[]) => TResult1 | PromiseLike<TResult1>) | null, onrejected?: ((reason: any) => TResult2 | PromiseLike<TResult2>) | null): Promise<TResult1 | TResult2> {
@@ -64,16 +60,6 @@ export class ExtendablePromise<T = unknown> /* extends Promise<T[]> */ implement
 
   [Symbol.toStringTag] = 'ExtendablePromise'
 }
-
-// // It's complicated..
-// Object.defineProperty(ExtendablePromise.prototype, Symbol.toStringTag, {
-//   get() { return 'ExtendablePromise' },
-//   enumerable: false,
-//   configurable: false,
-// })
-
-// TODO: Not good for performance!?
-// ExtendablePromise.prototype = Object.getPrototypeOf(Promise);
 
 export interface PromiseFulfilledResult<T> {
     status: "fulfilled";
